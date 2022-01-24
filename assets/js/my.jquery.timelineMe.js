8bd35bfb8988558bed60646435a87ab318241318
jennifer7733 <love890410@gmail.com>
2022-01-24 11:51:43 +0800

    my html

---
+++ assets/js/my.jquery.timelineMe.js
@@ -0,0 +1,37 @@
+$('#timeline-container-force-position').timelineMe({
+    items: [
+    {
+        type: 'smallItem',
+        label: '2017.06 - 至今',
+        shortContent: '臺安醫院 / 工讀生',
+        fullContent: '臺安醫院 / 工讀生：協助處理相關就醫流程，主動而適時給予親切熱忱之服務',
+        showMore: '詳細',
+        showLess: '摘要'
+    },
+    {
+        type: 'smallItem',
+        label: '2018.09 - 2019.06',
+        shortContent: '良建文理補習班 / 助理導師',
+        fullContent: '良建文理補習班 / 助理導師：處理負責國中班務行政工作、批改及協助指導國中生課業',
+        showMore: '詳細',
+        showLess: '摘要'
+    },
+    {
+        type: 'smallItem',
+        label: '2021.08 - 至今',
+        shortContent: '鎮麟文理補習班　/ 助理導師',
+        fullContent: '鎮麟文理補習班　/ 助理導師：處理負責高中班務行政工作 、高中課程介紹與解說',
+        showMore: '詳細',
+        showLess: '摘要',
+        forcePosition: 'right'
+    },
+    {
+        type: 'smallItem',
+        label: '2022.01 - 至今',
+        shortContent: '真常十童心圓文理短期補習班 / 國小助教',
+        fullContent: '真常十童心圓文理短期補習班 / 國小助教：協助班導師照顧學生、編輯講義、複習上課內容、製作營隊教案',
+        showMore: '詳細',
+        showLess: '摘要',
+        forcePosition: 'left'
+    }]
+});
---
+++ assets/js/my.mixitup.js
@@ -0,0 +1,4 @@
+$(function(){
+    var containerEl = document.querySelector('.container_mixeditup');
+    var mixer = mixitup(containerEl);
+})
